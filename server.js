275f259a48744167d00dab432fcd80bbc0f40ae2
krish <santtunray@gmail.com>
2025-04-05 05:09:34 +0530

    last try

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-const app = require("./src/app");
-module.exports = app; // Required for Vercel
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
